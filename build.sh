a2e25cafa249533d5cd21b290d3b6f3b654af53c
kapralos <evkapral@gmail.com>
2013-11-08 13:27:23 -0800

    1. Refactor \n2. Create builds against iOS SDK 7.0

--- build.sh
+++ build.sh
@@ -1,72 +1,83 @@
 #!/bin/bash
 
-# Yay shell scripting! This script builds a static version of
-# OpenSSL ${OPENSSL_VERSION} for iOS 5.1 that contains code for armv6, armv7 and i386.
+# This script builds a static version of OpenSSL ${OPENSSL_VERSION}
+# for the latest iOS containing binaries for armv7, armv7s and i386.
 
 set -x
 
-# Setup paths to stuff we need
+# Setup 
+OPENSSL_VERSION="1.0.1e"
 
-OPENSSL_VERSION="1.0.1c"
+ARCHS=("i386" "armv7" "armv7s")
+SDKS=("iphonesimulator" "iphoneos" "iphoneos")
+TMP_DIR="/tmp/openssl-${OPENSSL_VERSION}"
+OUTPUT_INCLUDE_DIR "include"
+OUTPUT_LIB_DIR "lib"
 
-DEVELOPER="/Applications/Xcode.app/Contents/Developer"
-
-SDK_VERSION="6.0"
-
-IPHONEOS_PLATFORM="${DEVELOPER}/Platforms/iPhoneOS.platform"
-IPHONEOS_SDK="${IPHONEOS_PLATFORM}/Developer/SDKs/iPhoneOS${SDK_VERSION}.sdk"
-IPHONEOS_GCC="${IPHONEOS_PLATFORM}/Developer/usr/bin/gcc"
-
-IPHONESIMULATOR_PLATFORM="${DEVELOPER}/Platforms/iPhoneSimulator.platform"
-IPHONESIMULATOR_SDK="${IPHONESIMULATOR_PLATFORM}/Developer/SDKs/iPhoneSimulator${SDK_VERSION}.sdk"
-IPHONESIMULATOR_GCC="${IPHONESIMULATOR_PLATFORM}/Developer/usr/bin/gcc"
-
-# Clean up whatever was left from our previous build
-
-rm -rf include lib
-rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*"
-rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*.log"
 
 build()
 {
    ARCH=$1
    GCC=$2
    SDK=$3
-   rm -rf "openssl-${OPENSSL_VERSION}"
-   tar xfz "openssl-${OPENSSL_VERSION}.tar.gz"
+   
    pushd .
    cd "openssl-${OPENSSL_VERSION}"
-   ./Configure BSD-generic32 --openssldir="/tmp/openssl-${OPENSSL_VERSION}-${ARCH}" &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
+   ./Configure BSD-generic32 --openssldir="${TMP_DIR}-${ARCH}" &> "${TMP_DIR}-${ARCH}.log"
    perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
    perl -i -pe "s|^CC= gcc|CC= ${GCC} -arch ${ARCH}|g" Makefile
    perl -i -pe "s|^CFLAG= (.*)|CFLAG= -isysroot ${SDK} \$1|g" Makefile
-   make &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
-   make install &> "/tmp/openssl-${OPENSSL_VERSION}-${ARCH}.log"
+   make &> "${TMP_DIR}-${ARCH}.log"
+   make install &> "${TMP_DIR}-${ARCH}.log"
    popd
-   rm -rf "openssl-${OPENSSL_VERSION}"
 }
 
-build "armv7" "${IPHONEOS_GCC}" "${IPHONEOS_SDK}"
-build "armv7s" "${IPHONEOS_GCC}" "${IPHONEOS_SDK}"
-build "i386" "${IPHONESIMULATOR_GCC}" "${IPHONESIMULATOR_SDK}"
+create_lib()
+{
+   LIB_SRC=$1;
+   LIB_DST=$2;
+   
+   LIB_PATHS=( "${ARCHS[@]/#/${TMP_DIR}-}" )
+   LIB_PATHS=( "${LIB_PATHS[@]/%//${LIB_SRC}}" )
+   lipo ${LIB_PATHS[@]} -create -output "${LIB_DST}"
+}
 
-#
 
-mkdir include
-cp -r /tmp/openssl-${OPENSSL_VERSION}-i386/include/openssl include/
+# Clean up whatever was left from previous build
+rm -rf ${OUTPUT_INCLUDE_DIR} ${OUTPUT_LIB_DIR}
+rm -rf "${TMP_DIR}-*"
+rm -rf "${TMP_DIR}-*.log"
+rm -rf "openssl-${OPENSSL_VERSION}"
 
-mkdir lib
-lipo \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libcrypto.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7s/lib/libcrypto.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-i386/lib/libcrypto.a" \
-	-create -output lib/libcrypto.a
-lipo \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7/lib/libssl.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-armv7s/lib/libssl.a" \
-	"/tmp/openssl-${OPENSSL_VERSION}-i386/lib/libssl.a" \
-	-create -output lib/libssl.a
+# Extract openssl sources
+tar xfz "openssl-${OPENSSL_VERSION}.tar.gz"
 
-rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*"
-rm -rf "/tmp/openssl-${OPENSSL_VERSION}-*.log"
+# Build
+for ((i=0; i < ${#ARCHS[@]}; i++))
+do
+   SDK_PATH=$(xcrun -sdk ${SDKS[i]} --show-sdk-path)
+   GCC=$(xcrun -sdk ${SDKS[i]} -find gcc)
+   build "${ARCHS[i]}" "${GCC}" "${SDK_PATH}"
+done
+
+# Clean up openssl sources
+rm -rf "openssl-${OPENSSL_VERSION}"
+
+# Copy headers
+mkdir ${OUTPUT_INCLUDE_DIR}
+cp -r ${TMP_DIR}-${ARCHS[0]}/include/openssl ${OUTPUT_INCLUDE_DIR}
+
+# Create lib
+mkdir ${OUTPUT_LIB_DIR}
+create_lib "lib/libcrypto.a" "${OUTPUT_LIB_DIR}/libcrypto.a"
+create_lib "lib/libssl.a" "${OUTPUT_LIB_DIR}/libssl.a"
+
+# If build succeeded, clean up tmp dir
+if [ -f lib/libcrypto.a ];
+then
+	rm -rf "${TMP_DIR}-*"
+	rm -rf "${TMP_DIR}-*.log"
+else
+	echo "Build failed, see the logs in /tmp for details"
+fi
 
